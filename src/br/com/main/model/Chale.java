dba6590bfa8ae20e64281ca0bb23ea51702579cd
Giovanni <gio.vannnni@hotmail.com>
2024-08-28 15:57:50 -0300

    feat: adding the Chale and Hospedagem entitys classes

--- src/br/com/main/model/Chale.java
+++ src/br/com/main/model/Chale.java
@@ -2,4 +2,60 @@ package br.com.main.model;
 
 public class Chale {
 
+	private Integer codChale;
+	private String localizacao;
+	private Integer capacidade;
+	private Double valorAltaEstacao;
+	private Double valorBaixaEstacao;
+
+	public Chale(Integer codChale, String localizacao, Integer capacidade, Double valorAltaEstacao,
+			Double valorBaixaEstacao) {
+		super();
+		this.codChale = codChale;
+		this.localizacao = localizacao;
+		this.capacidade = capacidade;
+		this.valorAltaEstacao = valorAltaEstacao;
+		this.valorBaixaEstacao = valorBaixaEstacao;
+	}
+
+	public Integer getCodChale() {
+		return codChale;
+	}
+
+	public void setCodChale(Integer codChale) {
+		this.codChale = codChale;
+	}
+
+	public String getLocalizacao() {
+		return localizacao;
+	}
+
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+
+	public Integer getCapacidade() {
+		return capacidade;
+	}
+
+	public void setCapacidade(Integer capacidade) {
+		this.capacidade = capacidade;
+	}
+
+	public Double getValorAltaEstacao() {
+		return valorAltaEstacao;
+	}
+
+	public void setValorAltaEstacao(Double valorAltaEstacao) {
+		this.valorAltaEstacao = valorAltaEstacao;
+	}
+
+	public Double getValorBaixaEstacao() {
+		return valorBaixaEstacao;
+	}
+
+	public void setValorBaixaEstacao(Double valorBaixaEstacao) {
+		this.valorBaixaEstacao = valorBaixaEstacao;
+	}
+
 }
--- src/br/com/main/model/Hospedagem.java
+++ src/br/com/main/model/Hospedagem.java
@@ -1,5 +1,119 @@
 package br.com.main.model;
 
-public class Hospedagem {
+import java.time.LocalDate;
 
+public class Hospedagem {
+    private Integer codHospedagem;
+    private Integer codChale;
+    private Integer codCliente;
+    private String estado;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private Integer qtdPessoas;
+    private Double desconto;
+    private Double valorFinal;
+
+    // Construtor padrão
+    public Hospedagem() {}
+
+    // Construtor com todos os campos
+    public Hospedagem(Integer codHospedagem, Integer codChale, Integer codCliente, String estado, LocalDate dataInicio, LocalDate dataFim, Integer qtdPessoas, Double desconto, Double valorFinal) {
+        this.codHospedagem = codHospedagem;
+        this.codChale = codChale;
+        this.codCliente = codCliente;
+        this.estado = estado;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.qtdPessoas = qtdPessoas;
+        this.desconto = desconto;
+        this.valorFinal = valorFinal;
+    }
+
+    // Getters e Setters
+    public Integer getCodHospedagem() {
+        return codHospedagem;
+    }
+
+    public void setCodHospedagem(Integer codHospedagem) {
+        this.codHospedagem = codHospedagem;
+    }
+
+    public Integer getCodChale() {
+        return codChale;
+    }
+
+    public void setCodChale(Integer codChale) {
+        this.codChale = codChale;
+    }
+
+    public Integer getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(Integer codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Integer getQtdPessoas() {
+        return qtdPessoas;
+    }
+
+    public void setQtdPessoas(Integer qtdPessoas) {
+        this.qtdPessoas = qtdPessoas;
+    }
+
+    public Double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(Double desconto) {
+        this.desconto = desconto;
+    }
+
+    public Double getValorFinal() {
+        return valorFinal;
+    }
+
+    public void setValorFinal(Double valorFinal) {
+        this.valorFinal = valorFinal;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospedagem{" +
+                "codHospedagem=" + codHospedagem +
+                ", codChale=" + codChale +
+                ", codCliente=" + codCliente +
+                ", estado='" + estado + '\'' +
+                ", dataInicio=" + dataInicio +
+                ", dataFim=" + dataFim +
+                ", qtdPessoas=" + qtdPessoas +
+                ", desconto=" + desconto +
+                ", valorFinal=" + valorFinal +
+                '}';
+    }
 }
