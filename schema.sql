d6e007e94b6082139baaa05ff6ef9d3fd579e5f7
gabriel <142801180+gcxz7@users.noreply.github.com>
2024-08-28 14:12:10 -0300

    fix: add constraint

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS "clientes" (
   "id" SERIAL PRIMARY KEY,
+  "nome" VARCHAR(64) NOT NULL,
   "rg" VARCHAR(20) NOT NULL,
   "endereco" VARCHAR(255),
   "bairro" VARCHAR(100),
@@ -15,6 +16,7 @@ CREATE TABLE IF NOT EXISTS "hospedagens" (
   "dataInicio" DATE NOT NULL,
   "dataFim" DATE NOT NULL,
   "desconto"  DECIMAL(10, 2),
+  "qtdPessoas" SMALLINTEGER,
   "valorFinal"  DECIMAL(10, 2) NOT NULL,
   "cliente_id" INTEGER REFERENCES "clientes"("id")
 );
