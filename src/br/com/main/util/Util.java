00226ea7ae75a2148a03b3e86bef79b3ec8569ab
gabriel <142801180+gcxz7@users.noreply.github.com>
2024-08-28 21:19:57 -0300

    feat: fetch implementation

--- src/br/com/main/util/Util.java
+++ src/br/com/main/util/Util.java
@@ -8,9 +8,10 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 import br.com.main.model.Cliente;
+import br.com.main.model.Hospedagem;
 
 public class Util {
-	public static void fetchUsers(JTable tblClients) {
+	public static void fetchClient(JTable tblClients) {
 		// TODO: fetch data
 		
 		List<Cliente> clients = new ArrayList<>();
@@ -26,7 +27,7 @@ public class Util {
 		
 		int row = 0;
 		for (Cliente client : clients) {
-            tbl.setValueAt(client.getCodClient(), row, 0);
+            tbl.setValueAt(client.getCodCliente(), row, 0);
             tbl.setValueAt(client.getNomeCliente(), row, 1);
             tbl.setValueAt(client.getRgCliente(), row, 2);
             tbl.setValueAt(client.getEnderecoCliente(), row, 3);
@@ -37,6 +38,35 @@ public class Util {
             tbl.setValueAt(client.getNascimentoCliente().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")), row, 8);
             
 			row++;
-		}		
+		}
 	}
+
+    public static void fetchStay(JTable tblStays) {
+        // TODO: fetch data
+        
+        List<Hospedagem> stays = new ArrayList<>();
+        
+        // HospedagemController hc = new HospedagemController();
+        // stays = hc.listAll();
+        
+        DefaultTableModel tbl = (DefaultTableModel) tblStays.getModel();
+        for (int i = tbl.getRowCount() - 1; i >= 0; i--) {
+            tbl.removeRow(i);
+        }
+        
+        int row = 0;
+        for (Hospedagem stay : stays) {
+            tbl.setValueAt(stay.getCodHospedagem(), row, 0);
+            tbl.setValueAt(stay.getCodChale(), row, 1);
+            tbl.setValueAt(stay.getCodCliente(), row, 2);
+            tbl.setValueAt(stay.getEstado(), row, 3);
+            tbl.setValueAt(stay.getDataInicio().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")), row, 4);
+            tbl.setValueAt(stay.getDataFim().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")), row, 5);
+            tbl.setValueAt(stay.getQtdPessoas(), row, 6);
+            tbl.setValueAt(stay.getDesconto(), row, 7);
+            tbl.setValueAt(stay.getValorFinal(), row, 8);
+            
+            row++;
+        }       
+    }
 }
